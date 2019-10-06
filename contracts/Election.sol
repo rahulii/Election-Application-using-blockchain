a8a982c9b0f8a077efeb6e04708ebae15029f486
rahulii <r.sawra@gmail.com>
2019-10-06 23:03:39 +0530

    2.List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,8 +1,28 @@
 pragma solidity 0.5.8;
 
 contract Election {
-string public candidate;
-constructor() public {
-    candidate = "Candidate 1";
-}
+    //model a candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //store candidates
+    //fetch candidates
+    mapping(uint => Candidate) public candidates;
+    //store candidate count
+    uint public candidateCount;
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount,_name,0);
+    }
+
+
+
 }
